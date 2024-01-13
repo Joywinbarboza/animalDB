829353d5329d555eb938a54782f69f10cfcb23d6
Joywinbarboza <joywinbarboza420@gmail.com>
2024-01-13 15:03:26 +0530

    first

---
+++ PROJECT/Backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '',
+    database: 'test'
+})
+
+
+//Checking by seeing the output at localhost:8081
+app.get('/',(req,res)=>{
+    return res.json("from Backend Side");
+});
+
+app.get('/animals',(req,res)=>{
+    //setting sql variable with this query 
+    const sql = "SELECT * FROM `animals_test`";
+
+    db.query(sql,(err,data)=>{
+        //if there is an error return a json with the error in it
+        if(err) return res.json(err);
+        
+        //else just return the result json with the data
+        return res.json(data);
+    })
+})
+
+//the port 8081 (i.e: localhost:8081) is made to listen
+app.listen(8081,()=>{
+    console.log("listening");
+})
---
+++ PROJECT/Frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8081/animals')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <div className='animal-container-main'>
+        {
+          data.map((d, i) => (
+            <div className="animal-container" key={i}>
+              <div className="animal-name">
+                <p>{d.name}</p>
+              </div>
+              <div className="animal-image">
+                <img src={'/images/animals/' + d.image_path} alt={d.image_path} />
+              </div>
+              <div className="animal-sc-name">
+                <p>{d.scientific_name}</p>
+              </div>
+              <div className="animal-fact">
+                <p>"{d.fact}"</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+      <p>hi</p>
+    </>
+  )
+
+}
+
+export default App

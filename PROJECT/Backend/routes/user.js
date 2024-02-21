16032c453a205f8dad2694a5ff864e4255b9c032
Anishabhandary <anishbhandari2203@gmail.com>
2024-02-21 18:54:57 +0530

    before organisation

---
+++ PROJECT/Backend/routes/user.js
@@ -0,0 +1,54 @@
+const express=require("express");
+const router=express.Router();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '',
+    database: 'animaldb'
+})
+
+
+//router for creating user
+router.post("/signup",(req,res)=>{
+    const sql="INSERT INTO users (`username`, `password_hash`, `email`) VALUES (?)";
+    const values=[
+        req.body.username,
+        req.body.password_hash,
+        req.body.email
+    ]
+
+    db.query(sql,[values],(err,data)=>{
+        if(err){
+            return res.json(err)
+        }
+
+        return res.json(data);
+    })
+})
+
+
+//router for login
+router.post("/login",(req,res)=>{
+    const sql="SELECT * FROM users where `email` =  ? AND `password_hash` = ?";
+    // const values=[
+    //     req.body.password_hash,
+    //     req.body.email
+    // ]
+
+    db.query(sql,[req.body.email,req.body.password_hash],(err,data)=>{
+        if(err){
+            return res.json(err)
+        }
+        // return res.json(data);
+        if(data.length>0){
+            return res.status(200).json(data)
+        }
+        else{
+            return res.json("No such user exists")
+        }
+    })
+})
+
+module.exports=router;
--- PROJECT/Frontend/src/Components/Home.jsx
+++ PROJECT/Frontend/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ImageSlider from "./ImageSlider";
+import Book from "./Book";
 
 function Home() {
   const [data, setData] = useState([]);
@@ -48,6 +49,7 @@ function Home() {
       </div> */}
       <div>
         <ImageSlider slides={data}/>
+        <Book/>
       </div>
     </>
   );
